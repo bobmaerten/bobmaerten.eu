771abce36a1ea150578efafc97d1b515f00f8810
Bob Maerten <bob.maerten@gmail.com>
2015-05-27 22:33:39 +0200

    style: remove comments and whitespaces

--- config.rb
+++ config.rb
@@ -21,9 +21,8 @@ activate :syntax
 activate :protect_emails
 # activate :automatic_image_sizes
 
-helpers do  
+helpers do
   def card_image_url(source)
-    # 'https://bobmaerten.eu' + image-url(source)
     path = image_path("#{source}.jpg")
     if path.start_with? '..'
       path = path[3..-1]
